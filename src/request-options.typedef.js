07850333126cc5007b0a86e7052b03dc23ad02d8
David Chin <davidchin.yf@gmail.com>
2017-12-27 15:14:05 +1100

    chore(request): CHECKOUT-2738 Update `RequestOptions` typedef

--- src/request-options.typedef.js
+++ src/request-options.typedef.js
@@ -4,6 +4,6 @@
  * @property {?Object} headers
  * @property {?Object} params
  * @property {?string} method
- * @property {?string} credentials
+ * @property {?boolean} credentials
  * @property {?Timeout} timeout
  */
